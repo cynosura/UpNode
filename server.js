0a61365f4b8a20092674b204dc7672ee18ff6f41
Ray Glover <Ray@NAUSICA>
2013-07-06 21:19:17 +0100

    fix indents

--- server.js
+++ server.js
@@ -26,178 +26,176 @@ var uploadsPath = __dirname + config.uploadsPath
 // server setup
 http.createServer(function(req, res) {
 
-   //
-   // prevent double requests
-   // https://gist.github.com/763822
-   //
-   if (req.url === '/favicon.ico') {
-      res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-      res.end();
-      return;
-   }
+  //
+  // prevent double requests
+  // https://gist.github.com/763822
+  //
+  if (req.url === '/favicon.ico') {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end();
+    return;
+  }
 
-   //
-   // ROUTER
-   // setup simple RESTful routing
-   //
-   var method = req.method.toLowerCase();
+  //
+  // ROUTER
+  // setup simple RESTful routing
+  //
+  var method = req.method.toLowerCase();
 
-   if (method === 'get') {
-      serveFile(req, res);
-   } else if (method === 'post') {
-      saveFile(req, res);
-   }
+  if (method === 'get') {
+    serveFile(req, res);
+  } else if (method === 'post') {
+    saveFile(req, res);
+  }
 
-   // --------------------
+  // --------------------
 
-   //
-   // GET request
-   // serve the requested file if it exists
-   //
-   function serveFile(req, res) {
-      var uri = url.parse(req.url).pathname
-        , filename = path.join(uploadsPath, uri);
-  
-      fs.exists(filename, function(exists) {
+  //
+  // GET request
+  // serve the requested file if it exists
+  //
+  function serveFile(req, res) {
+    var uri = url.parse(req.url).pathname
+      , filename = path.join(uploadsPath, uri);
 
-         // check if file exists
-         if(!exists) {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('404 Not Found\n' + filename + ' does not exist.');
-            return;
-         }
+    fs.exists(filename, function(exists) {
 
-         if (fs.statSync(filename).isDirectory()) {
-            // file is directory, output directory listing as json
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(fs.readdirSync(filename)));
-         } else {
-           // serve file
-           fs.readFile(filename, 'binary', function(err, file) {
-              if(err) {
-                 res.writeHead(500, {'Content-Type': 'text/plain'});
-                 res.end(err);
-                 return;
-              }
-
-              // lookup correct mime type
-              var mimeType = mime.lookup(filename);
-
-              res.writeHead(200, {'Content-Type': mimeType});
-              res.end(file, 'binary');
-           });
-         }
-      });
-   }
-
-   // --------------------
-
-   //
-   // POST request
-   // save the given files to disk
-   //
-   function saveFile(req, res) {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-
-      // map url to path
-      var url = require('url').parse( req.url, false )
-        , pathname = path.join( '/', url.pathname )
-        , dirname;
-        
-      // if the path ends with '/', treat url as directory location
-      if( pathname.lastIndexOf( path.sep ) === pathname.length - 1 ) {
-         dirname = pathname;
-         filename = null;
-      } else {
-         dirname = path.dirname( pathname );
-         filename = path.basename( url.pathname );
+      // check if file exists
+      if(!exists) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 Not Found\n' + filename + ' does not exist.');
+        return;
       }
 
-      // JSON return object
-      var json = {};
+      if (fs.statSync(filename).isDirectory()) {
+        // file is directory, output directory listing as json
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(fs.readdirSync(filename)));
+      } else {
+       // serve file
+       fs.readFile(filename, 'binary', function(err, file) {
+          if(err) {
+             res.writeHead(500, {'Content-Type': 'text/plain'});
+             res.end(err);
+             return;
+          }
 
-      // JSON defaults
-      json.errors = false;
-      json.numberOfFiles = 0;
-      json.files = [];
+          // lookup correct mime type
+          var mimeType = mime.lookup(filename);
 
-      // parse file upload
-      var form = new Formidable.IncomingForm()
-        , progress = null
-        , bytesAlreadyReceived = 0
-        , files = []
-        , fields = {};
+          res.writeHead(200, {'Content-Type': mimeType});
+          res.end(file, 'binary');
+       });
+      }
+    });
+  }
 
-      form
-         // error
-         .on('error', function(err) {
-            json.error = err;
-            res.end(JSON.stringify(json));
-         })
+  // --------------------
 
-         // set the file path
-         .on('fileBegin', function(name, file) {
-            var dirpath = path.join( uploadsPath, dirname );
-            
-            // create dir path if it doesn't already exist
-            moreFs.mkdirSync( dirpath, null, true /* recursive */ );
-            
-            // if request path doesnt contain file name, use name of the uploaded file
-            file.path = path.join( dirpath, filename || file.name );
-            process.stdout.write('File: ' + file.path + '\n');
-         })
+  //
+  // POST request
+  // save the given files to disk
+  //
+  function saveFile(req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
 
-         // progress
-         .on('progress', function(bytesReceived, bytesExpected) {
-            var percent = ((bytesReceived/bytesExpected)*100).toFixed(2);
-            process.stdout.write('\033[2K' + 'Uploading: ' + percent + '%');
-         })
+    // map url to path
+    var url = require('url').parse( req.url, false )
+      , pathname = path.join( '/', url.pathname )
+      , dirname;
+      
+    // if the path ends with '/', treat url as directory location
+    if( pathname.lastIndexOf( path.sep ) === pathname.length - 1 ) {
+      dirname = pathname;
+      filename = null;
+    } else {
+      dirname = path.dirname( pathname );
+      filename = path.basename( url.pathname );
+    }
 
-         // incoming file
-         .on('file', function(field, file) {
+    // JSON return object
+    var json = {};
 
-            // Check file for allowed/whitelisted files
-            var mimeType = mime.lookup(file.name);
-            if (typeof(mimeTypeWhitelist) !== 'undefined' && mimeTypeWhitelist.length > 0) {
-               
-               // actually use the whitelist
-               if(mimeTypeWhitelist.indexOf(mimeType) == -1) {
-                  // error, the file's mime is not white listed
-                  console.log("\nIgnoring uploaded file '" + file.name + "' of type '" + mimeType + "', the file's mime type is not white listed.");
-                  return;
-               }
-            }
+    // JSON defaults
+    json.errors = false;
+    json.numberOfFiles = 0;
+    json.files = [];
 
-            // add file to files list
-            var fileInfo = {
-              name:             file.name,
-              size:             file.size,
-              pathname:         path.relative( uploadsPath, file.path ),
-              lastModifiedDate: file.lastModifiedDate,
-              mimeType:         mimeType
-            };
+    // parse file upload
+    var form = new Formidable.IncomingForm()
+      , files = []
+      , fields = {};
 
-            files.push(fileInfo);
-         })
+    form
+      // error
+      .on('error', function(err) {
+        json.error = err;
+        res.end(JSON.stringify(json));
+      })
 
-         // fields, other query params
-         .on('field', function(name, value) {
-            fields[name] = value;
-         }) 
+      // set the file path
+      .on('fileBegin', function(name, file) {
+        var dirpath = path.join( uploadsPath, dirname );
+        
+        // create dir path if it doesn't already exist
+        moreFs.mkdirSync( dirpath, null, true /* recursive */ );
+        
+        // if request path doesnt contain file name, use name of the uploaded file
+        file.path = path.join( dirpath, filename || file.name );
+        process.stdout.write('File: ' + file.path + '\n');
+      })
 
-         // end of request
-         .on('end', function() {
-            //json.params = params;
-            json.fields = fields;
-            json.files = files;
-            json.numberOfFiles = files.length;
-            res.end(JSON.stringify(json));
+      // progress
+      .on('progress', function(bytesReceived, bytesExpected) {
+        var percent = ((bytesReceived/bytesExpected)*100).toFixed(2);
+        process.stdout.write('\033[2K' + 'Uploading: ' + percent + '%');
+      })
 
-            process.stdout.write('\r\033[2K' + 'Uploading: [DONE]\n');
+      // incoming file
+      .on('file', function(field, file) {
 
-         });
-      form.parse(req);
-   }
+        // Check file for allowed/whitelisted files
+        var mimeType = mime.lookup(file.name);
+        if (typeof(mimeTypeWhitelist) !== 'undefined' && mimeTypeWhitelist.length > 0) {
+           
+           // actually use the whitelist
+           if(mimeTypeWhitelist.indexOf(mimeType) == -1) {
+              // error, the file's mime is not white listed
+              console.log("\nIgnoring uploaded file '" + file.name + "' of type '" + mimeType + "', the file's mime type is not white listed.");
+              return;
+           }
+        }
+
+        // add file to files list
+        var fileInfo = {
+          name:             file.name,
+          size:             file.size,
+          pathname:         path.relative( uploadsPath, file.path ),
+          lastModifiedDate: file.lastModifiedDate,
+          mimeType:         mimeType
+        };
+
+        files.push(fileInfo);
+      })
+
+      // fields, other query params
+      .on('field', function(name, value) {
+        fields[name] = value;
+      }) 
+
+      // end of request
+      .on('end', function() {
+        //json.params = params;
+        json.fields = fields;
+        json.files = files;
+        json.numberOfFiles = files.length;
+        res.end(JSON.stringify(json));
+
+        process.stdout.write('\r\033[2K' + 'Uploading: [DONE]\n');
+
+      });
+    form.parse(req);
+  }
 
 }).listen(parseInt(port, 10), 'localhost');
 
